3abf7b7db75c1f125a94c2db0a603227160272ae
imsid22 <siddhantd94@gmail.com>
2023-10-14 21:38:57 -0600

    feat: Implemented the navigation bar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,11 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-// import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setDarkMode(prevDarkMode => !prevDarkMode);
-  };
-
-  return (
-    <div className={`flex flex-col min-h-screen ${darkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
-      <Header toggleTheme={toggleTheme} darkMode={darkMode} />
-      <div className="flex-grow flex items-center justify-center">
-        {/* ... rest of your app content */}
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
 }
 
 export default App;
--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const NavBar = () => {
 
-    const linkArr = [
-        {
-            id: 1,
-            link: 'home'
-        },
-        {
-            id: 2,
-            link: 'about'
-        },
-        {
-            id: 3,
-            link: 'portfolio'
-        },
-        {
-            id: 4,
-            link: 'experience'
-        },
-        {
-            id: 5,
-            link: 'contact'
-        }
-    ]
+    const [nav, setNav] = useState(false);
+    const handleClick = () => setNav(!nav);
 
     return (
-    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+    <div className='flex justify-between items-center w-full h-[80px] px-4 text-white bg-black fixed'>
         
         <div>
-            <h1 className='text-5xl font-signature ml-2'>Siddhant</h1>
+            <h1>Toggle Switch Here</h1>
         </div>
 
+        {/* Menu */}
         <ul className='hidden md:flex'>
+            <li className='cursor-pointer hover:scale-105 duration-200'>Home</li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>About</li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>Experience</li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>Work</li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>Contact</li>
+        </ul>
+
+
+        {/* Hamburger */}
+        <div onClick={handleClick} className='md:hidden z-10'> 
+            {!nav ? <FaBars/> : <FaTimes/>}
+        </div>
+        
+        {/* Mobile Menu */}
+        
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>Home</li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>About</li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>Experience</li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>Work</li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>Contact</li>
+        </ul>
+        
+        {/* Social Icons */}
+
+        <div className='hidden'> </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        {/* <ul className='hidden md:flex'>
 
             {linkArr.map(({id, link}) => (
                 <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200'>
@@ -42,9 +64,11 @@ const NavBar = () => {
             ))}
 
         </ul>
-    
+        <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500'>
+            <FaBars size={30} />
+        </div> */}
     </div>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
