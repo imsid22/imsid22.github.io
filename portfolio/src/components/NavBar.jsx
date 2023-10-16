feca70f74baae83ce1fbf2e1d26288fb756100e7
imsid22 <siddhantd94@gmail.com>
2023-10-15 19:45:04 -0600

    feat: added scrolling to link to the respective sections

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 const NavBar = () => {
 
@@ -17,11 +18,31 @@ const NavBar = () => {
 
         {/* Menu */}
         <ul className='hidden md:flex'>
-            <li className='cursor-pointer hover:scale-105 duration-200'>Home</li>
-            <li className='cursor-pointer hover:scale-105 duration-200'>About</li>
-            <li className='cursor-pointer hover:scale-105 duration-200'>Skills</li>
-            <li className='cursor-pointer hover:scale-105 duration-200'>Projects</li>
-            <li className='cursor-pointer hover:scale-105 duration-200'>Contact</li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>
+                <Link to="home" smooth={true} duration={500}>
+                  Home
+                </Link>
+            </li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>
+                <Link to="about" smooth={true} duration={500}>
+                  About
+                </Link>
+            </li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>
+                <Link to="skills" smooth={true} duration={500}>
+                  Skills
+                </Link>
+            </li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>
+                <Link to="projects" smooth={true} duration={500}>
+                  Projects
+                </Link>
+            </li>
+            <li className='cursor-pointer hover:scale-105 duration-200'>
+                <Link to="contact" smooth={true} duration={500}>
+                  Contact
+                </Link>
+            </li>
         </ul>
 
 
@@ -33,11 +54,31 @@ const NavBar = () => {
         {/* Mobile Menu */}
         
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>Home</li>
-            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>About</li>
-            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>Skills</li>
-            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>Projects</li>
-            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>Contact</li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>
+                <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                  Home
+                </Link>
+            </li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>
+                <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                  About
+                </Link>
+            </li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>
+                <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                  Skills
+                </Link>
+            </li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>
+                <Link onClick={handleClick} to="projects" smooth={true} duration={500}>
+                  Projects
+                </Link>
+            </li>
+            <li className='py-6 text-4xl cursor-pointer hover:scale-105 duration-200'>
+                <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                  Contact
+                </Link>
+            </li>
         </ul>
         
         {/* Social Icons */}
